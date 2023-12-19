3746cbee5570880c5b5f8fa5ea91c2d5a33c4fa7
melania7730 <150779229+melania7730@users.noreply.github.com>
2023-12-19 16:21:16 +0700

    Added search function using API

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
+function updatedWeather(response) {
+  let temperatureNumber = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let weatherCity = document.querySelector("#weather-city");
+  weatherCity.innerHTML = response.data.city;
+  temperatureNumber.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "b5dca18t5009430fdddbc6b9afdb4d2o";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(updatedWeather);
+}
+
 function searchResult(event) {
   event.preventDefault();
-  let weatherCity = document.querySelector("#weather-city");
   let formCity = document.querySelector("#form-city");
-  weatherCity.innerHTML = formCity.value;
+  searchCity(formCity.value);
 }
 let searchForm = document.querySelector("#form-search");
 searchForm.addEventListener("submit", searchResult);
+
+searchCity("Denpasar");
