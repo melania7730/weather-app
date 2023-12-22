e16f782c3a24579796b82100f85b7fc87db6d0f4
melania7730 <150779229+melania7730@users.noreply.github.com>
2023-12-22 15:58:03 +0700

    Added more weather data

--- script.js
+++ script.js
@@ -2,8 +2,36 @@ function updatedWeather(response) {
   let temperatureNumber = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let weatherCity = document.querySelector("#weather-city");
-  weatherCity.innerHTML = response.data.city;
+  let weatherDescription = document.querySelector("#description");
+  let weatherHumidity = document.querySelector("#humidity");
+  let windSpeed = document.querySelector("#wind-speed");
+  let time = document.querySelector("time");
+  let date = new Day(response.data.time * 1000);
   temperatureNumber.innerHTML = Math.round(temperature);
+  weatherCity.innerHTML = response.data.city;
+  time.innerHTML = formatDate(date);
+  weatherDescription.innerHTML = response.data.condition.description;
+  weatherHumidity.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeed.innerHTML = `${response.data.wind.speed} km/h`;
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getday()];
+  return `${days}, ${hours}:${minutes}`;
+  if (minutes < 10) {
+    minutes`0${minutes}`;
+  }
 }
 
 function searchCity(city) {
